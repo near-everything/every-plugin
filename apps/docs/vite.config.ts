90516a983334b8cdb90e1386962a69734f3aef99
Elliot Braem <elliot@ejlbraem.com>
2025-10-20 13:52:00 -0500

    upgrade docs

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -1,5 +1,5 @@
-import netlify from '@netlify/vite-plugin-tanstack-start';
 import tailwindcss from '@tailwindcss/vite';
+import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import react from '@vitejs/plugin-react';
 import mdx from 'fumadocs-mdx/vite';
@@ -21,7 +21,10 @@ export default defineConfig({
         enabled: true,
       },
     }),
-    netlify(),
+    nitroV2Plugin({
+      preset: 'bun',
+      compatibilityDate: "2025-10-20"
+    }),
     react(),
   ],
 });
